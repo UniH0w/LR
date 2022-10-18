75dc165e69c18ca98e76ae52d67e02a45d4e3e6e
Ilya Zhurin <92648539+UniH0w@users.noreply.github.com>
2022-10-18 18:27:42 +0300

    Lr-1

--- lr1-1/lr1-1/Controllers/WeatherForecastController.cs
+++ lr1-1/lr1-1/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace lr1_1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            //
             /*_repository.Company.AnyMethodFromCompanyRepository();
             _repository.Employee.AnyMethodFromEmployeeRepository();*/
             return new string[] { "value1", "value2" };
